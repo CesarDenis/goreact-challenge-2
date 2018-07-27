f413cbc31f3bdd060a853d8f51b4966bd5eacbe9
Cesar Denis <cesar@cesardenis.com>
2018-07-27 11:35:35 -0300

    feature: add font awesome 4

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,16 @@
 import { injectGlobal } from 'styled-components';
 
+import 'font-awesome/css/font-awesome.css';
+
 injectGlobal`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    &:focus {
+      outline: 0;
+    }
   }
 
   body {
