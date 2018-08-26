ebfe41dd25e686e4db23607cbe2f8d5c6a29b4c0
Cesar Denis <cesar@cesardenis.com>
2018-08-26 14:28:29 -0300

    refactor: change sidebar and main page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 import Form from '../Form';
 import RepositoriesList from '../RepositoriesList';
 
 import Aside from './style';
 
-const Sidebar = ({
-  loading,
-  repositoryInput,
-  repositoryError,
-  repositories,
-  handleChangeInput,
-  handleAddRepository,
-  handleSelectRepository,
-}) => (
-  <Aside>
-    <Form
-      loading={loading}
-      repositoryInput={repositoryInput}
-      repositoryError={repositoryError}
-      handleChangeInput={handleChangeInput}
-      handleAddRepository={handleAddRepository}
-    />
-    <RepositoriesList repositories={repositories} handleSelectRepository={handleSelectRepository} />
-  </Aside>
-);
+class Sidebar extends Component {
+  static propTypes = {
+    handleSelectRepository: PropTypes.func.isRequired,
+  };
 
-Sidebar.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  repositoryInput: PropTypes.string.isRequired,
-  repositoryError: PropTypes.bool.isRequired,
-  repositories: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      owner: PropTypes.shape({
-        login: PropTypes.string,
-        avatar_url: PropTypes.string,
-      }),
-    })
-  ).isRequired,
-  handleChangeInput: PropTypes.func.isRequired,
-  handleAddRepository: PropTypes.func.isRequired,
-  handleSelectRepository: PropTypes.func.isRequired,
-};
+  state = {
+    loading: false,
+    repositoryInput: '',
+    repositoryError: false,
+    repositories: [],
+  };
+
+  handleChangeInput = e => {
+    this.setState({ repositoryInput: e.target.value });
+  };
+
+  handleAddRepository = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    try {
+      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      this.setState({
+        repositoryInput: '',
+        repositoryError: false,
+        repositories: [...this.state.repositories, response.data],
+      });
+    } catch (err) {
+      this.setState({
+        repositoryError: true,
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const { loading, repositoryInput, repositoryError, repositories } = this.state;
+    const { handleSelectRepository } = this.props;
+    return (
+      <Aside>
+        <Form
+          loading={loading}
+          repositoryInput={repositoryInput}
+          repositoryError={repositoryError}
+          handleChangeInput={this.handleChangeInput}
+          handleAddRepository={this.handleAddRepository}
+        />
+        <RepositoriesList
+          repositories={repositories}
+          handleSelectRepository={handleSelectRepository}
+        />
+      </Aside>
+    );
+  }
+}
 
 export default Sidebar;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,44 +1,13 @@
 import React, { Component } from 'react';
-import api from '../../services/api';
 
 import Sidebar from '../../components/Sidebar';
-import Issues from '../../components/Issues';
+import IssuesList from '../../components/IssuesList';
 
 import Container from './style';
 
 class Main extends Component {
   state = {
-    loading: false,
-    repositoryInput: '',
-    repositoryError: false,
     repositorySelected: {},
-    repositories: [],
-  };
-
-  handleChangeInput = e => {
-    this.setState({ repositoryInput: e.target.value });
-  };
-
-  handleAddRepository = async e => {
-    e.preventDefault();
-
-    this.setState({ loading: true });
-
-    try {
-      const response = await api.get(`/repos/${this.state.repositoryInput}`);
-
-      this.setState({
-        repositoryInput: '',
-        repositoryError: false,
-        repositories: [...this.state.repositories, response.data],
-      });
-    } catch (err) {
-      this.setState({
-        repositoryError: true,
-      });
-    } finally {
-      this.setState({ loading: false });
-    }
   };
 
   handleSelectRepository = ({ repository, e }) => {
@@ -50,16 +19,8 @@ class Main extends Component {
   render() {
     return (
       <Container>
-        <Sidebar
-          loading={this.state.loading}
-          repositoryInput={this.state.repositoryInput}
-          repositoryError={this.state.repositoryError}
-          repositories={this.state.repositories}
-          handleChangeInput={this.handleChangeInput}
-          handleAddRepository={this.handleAddRepository}
-          handleSelectRepository={this.handleSelectRepository}
-        />
-        <Issues repositorySelected={this.state.repositorySelected} />
+        <Sidebar handleSelectRepository={this.handleSelectRepository} />
+        <IssuesList repositorySelected={this.state.repositorySelected} />
       </Container>
     );
   }
